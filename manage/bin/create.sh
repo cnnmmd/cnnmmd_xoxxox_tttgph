3fb4c03f797953a038ee307e63dec631299860c6
azsgit <10238943+azsgit@users.noreply.github.com>
2026-05-06 15:39:19 +0900

    reorganize code

--- manage/bin/create.sh
+++ manage/bin/create.sh
@@ -5,4 +5,10 @@ source "${pthtop}"/manage/lib/params.sh
 source "${pthtop}"/manage/lib/shared.sh
 source "${pthcrr}"/params.sh
 
+pthkey="${HOME}"/sec/key/.envath_oai.txt
+
+if test ! -e "${pthkey}"
+then
+  cnfrtn "manual: ${pthkey}: OPENAI_API_KEY=XXX"
+fi
 addimg ${imgtgt} "${cnfimg}" "${pthdoc}"
